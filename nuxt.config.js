a12e9ba990d6ddc50d85a94ff82182e7f47130b7
NLGRF <non.devtech@gmail.com>
2019-10-04 22:02:12 +0700

    DatePicker

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/moment.js'
+    '~/plugins/moment.js',
+    { src: '~/plugins/datePicker.js', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ store/index.js
@@ -0,0 +1,9 @@
+import address from './address'
+
+export const state = () => ({
+  ...address.state,
+})
+
+export const mutations = {
+  ...address.mutations,
+}
